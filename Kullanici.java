7e978005e32201e0dd947b50ba85493bca1cefde
Mehmet Aydın <54308525+kocaeliproje@users.noreply.github.com>
2024-12-24 08:36:17 +0300

    Update Kullanici.java
    Kullanım kolaylığı için güncellendi.

--- Kullanici.java
+++ Kullanici.java
@@ -1,43 +1,34 @@
-class Kullanici {
+public class Kullanici{
     private String id;
     private String sifre;
     private String ad;
     private String soyad;
     private String iban;
     private Hesap hesap;
-    private Kart kart;
 
-    public Kullanici(String id, String sifre, String ad, String soyad, String iban, Hesap hesap, Kart kart) {
+    public Kullanici(String id, String sifre, String ad, String soyad, String iban, Hesap hesap) {
         this.id = id;
         this.sifre = sifre;
         this.ad = ad;
         this.soyad = soyad;
         this.iban = iban;
         this.hesap = hesap;
-        this.kart = kart;
     }
 
     public String getSifre() {
-        return this.sifre;
+        return sifre;
     }
-
     public String getAd() {
-        return this.ad;
+        return ad;
     }
-
     public String getSoyad() {
-        return this.soyad;
+        return soyad;
     }
-
     public String getIban() {
-        return this.iban;
+        return iban;
     }
-
     public Hesap getHesap() {
-        return this.hesap;
+        return hesap;
     }
 
-    public Kart getKart() {
-        return this.kart;
-    }
 }
