ab84ad61957d4ca1346715ff4865b7ff38b2a66e
Mehmet Aydın <54308525+kocaeliproje@users.noreply.github.com>
2024-12-30 10:08:33 +0300

    Update Kullanici.java
    Güncelleme

--- Kullanici.java
+++ Kullanici.java
@@ -1,10 +1,12 @@
-public class Kullanici{
+public class Kullanici {
     private String id;
     private String sifre;
     private String ad;
     private String soyad;
     private String iban;
     private Hesap hesap;
+    private DovizPozisyonu dovizPozisyonu;
+    private SifreYoneticisi sifreYoneticisi;
 
     public Kullanici(String id, String sifre, String ad, String soyad, String iban, Hesap hesap) {
         this.id = id;
@@ -13,22 +15,18 @@ public class Kullanici{
         this.soyad = soyad;
         this.iban = iban;
         this.hesap = hesap;
+        this.dovizPozisyonu = new DovizPozisyonu();
+        this.sifreYoneticisi = new SifreYoneticisi(sifre);
+    }
+    public boolean sifreDogrula(String girilenSifre) {
+        return sifreYoneticisi.sifreDene(girilenSifre);
     }
 
-    public String getSifre() {
-        return sifre;
-    }
-    public String getAd() {
-        return ad;
-    }
-    public String getSoyad() {
-        return soyad;
-    }
-    public String getIban() {
-        return iban;
-    }
-    public Hesap getHesap() {
-        return hesap;
-    }
 
+    public String getSifre() { return sifre; }
+    public String getAd() { return ad; }
+    public String getSoyad() { return soyad; }
+    public String getIban() { return iban; }
+    public Hesap getHesap() { return hesap; }
+    public DovizPozisyonu getDovizPozisyonu() { return dovizPozisyonu; }
 }
